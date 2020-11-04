821c67fe11650ffe2f27bdcd23c1f7ed17313723
李志鹏 <lizhipeng@bobandata.com>
2020-11-04 16:06:41 +0800

    1、修改打包方法 2、修改logging 为 slf4j

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import com.bobandata.iot.basedb.BaseApplication;
+
+/**
+ * @Author: 李志鹏
+ * @Date: 2020/11/4 10:50
+ * @Desc:
+ * @param:
+ * @return:
+ **/
+public class Main {
+    public static void main(String[] args) {
+        BaseApplication.main(args);
+    }
+}
--- src/main/java/com/bobandata/iot/basedb/BaseApplication.java
+++ src/main/java/com/bobandata/iot/basedb/BaseApplication.java
@@ -13,13 +13,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 @EntityScan(basePackages = "com.bobandata.iot.entity.dms")
 public class BaseApplication extends AbstractJavaFxApplicationSupport {
-    private static SplashScreen splashScreen;
-    private static Class<? extends AbstractFxmlView> savedInitialView;
-    private static BooleanProperty appCtxLoaded = new SimpleBooleanProperty(false);
-
     public static void main(String[] args) {
-        splashScreen = new CustomSplash();
-        savedInitialView = MainWebView.class;
-        launchApp(BaseApplication.class, MainWebView.class, splashScreen, args);
+        launchApp(BaseApplication.class, MainWebView.class, new CustomSplash(), args);
     }
 }
